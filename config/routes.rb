c518a5cd9fa91f4a3fa9e75bb6753b32e229c07a
Zsuzsanna Orehoczki <ore.zsuzsanna@gmail.com>
2022-10-15 14:15:16 +0100

    created task manager

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,20 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "tasks", to: "tasks#all"
+
+  get "tasks/new", to: "tasks#new"
+
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  post "tasks", to: "tasks#create"
+
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+
+  patch "tasks/:id", to: "tasks#update"
+
+  delete "tasks/:id", to: "tasks#destroy"
+
+
+
 end
